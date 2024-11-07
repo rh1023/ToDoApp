ff1732d24502435ee790152684e7dce70af67364
rh1023 <rh10230810+se@gmail.com>
2024-11-07 16:00:21 +0900

    update：画面レイアウト

--- src/resources/views/taskadd.blade.php
+++ src/resources/views/taskadd.blade.php
@@ -1,3 +1,12 @@
+{{--
+2024.11.07  15時半  Bootstrapを導入
+--}}
+
+<head>
+    <title>タスク入力</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -10,57 +19,108 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
 
-                    <h1>タスク入力フォーム</h1>
-
                     <div class="">
                         <label for="text_input" class="">タスク名入力</label>
                         <input type="text" class="form-control" id="text_input" name="text_input" required>
                     </div>
 
-                    <div class="">
-                        <label for="dropdown" class="">カテゴリ</label>
-                        <select class="" id="dropdown" name="dropdown" required>
-                            <option value="">選択してください</option>
-                            <option value="option1">家事</option>
-                            <option value="option2">仕事</option>
-                            <option value="option3">健康</option>
-                            <option value="option4">自己研鑽</option>
-                            <option value="option5">趣味</option>
-                            <option value="option6">その他</option>
-                        </select>
+                    <br>
+
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="card">
+                                <div class="card-body">
+                                    <label for="text_input" class="">カテゴリ</label>
+                                    <select class="dropdown" id="dropdown" name="dropdown" required>
+                                        <option value="">選択してください</option>
+                                        <option value="option1">家事</option>
+                                        <option value="option2">仕事</option>
+                                        <option value="option3">健康</option>
+                                        <option value="option4">自己研鑽</option>
+                                        <option value="option5">趣味</option>
+                                        <option value="option6">その他</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-6">
+                            <div class="card">
+                                <div class="card-body">
+                                    <label for="text_input" class="">重要度</label>
+                                    <select class="dropdown" id="dropdown" name="dropdown" required>
+                                        <option value="">選択してください</option>
+                                        <option value="option1">1</option>
+                                        <option value="option2">2</option>
+                                        <option value="option3">3</option>
+                                        <option value="option4">4</option>
+                                        <option value="option5">5</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+
+                    <br>
 
                     <div class="">
                         <label for="text_input" class="">期日入力</label>
-                        <div class="">
-                            <label for="date_input" class="form-label">本日</label>
-                            <input type="date" class="form-control" id="date_input" name="date_input" required>
-                        </div>
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title">本日</h5>
+                                        <input type="date" class="form-control" id="date_input" name="date_input"
+                                            required>
+                                    </div>
+                                </div>
+                            </div>
 
-                        <div class="">
-                            <label for="date_input" class="form-label">日付選択</label>
-                            <input type="date" class="form-control" id="date_input" name="date_input" required>
-                        </div>
+                            <div class="col-sm-4">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title">日付選択</h5>
+                                        <input type="date" class="form-control" id="date_input" name="date_input"
+                                            required>
+                                    </div>
+                                </div>
+                            </div>
 
-                        <div>
-                            <label for="start_date">開始日：</label>
-                            <input type="date" id="start_date" name="start_date">
+                            <div class="col-sm-4">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title">期間選択</h5>
+                                        <label for="start_date">開始日：</label>
+                                        <input type="date" id="start_date" name="start_date">
+                                        <br>
+                                        <label for="end_date">終了日：</label>
+                                        <input type="date" id="end_date" name="end_date">
+                                    </div>
+                                </div>
 
-                            <label for="end_date">終了日：</label>
-                            <input type="date" id="end_date" name="end_date">
+                            </div>
                         </div>
                     </div>
+
+                    <br>
 
                     <div class="">
                         <label for="text_area" class="form-label">テキスト詳細入力</label>
                         <textarea class="form-control" id="text_area" name="text_area" rows="3" required></textarea>
                     </div>
 
-                    <div class="">
-                        <button type="button" class="" onclick="history.back()">戻る</button>
-                        <button type="submit" class="">保存</button>
-                    </div>
+                    <br>
+
+                    <form action="{{ route('tasklist.taskshow') }}">
+                        <input type="submit" class="btn btn-primary" value="戻る">
                     </form>
+
+                    <br>
+
+                    <form action="{{ route('tasklist.taskshow') }}">
+                        <input type="submit" class="btn btn-primary" value="保存">
+                    </form>
+
 
                 </div>
             </div>
@@ -69,3 +129,7 @@
     </div>
 
 </x-app-layout>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
+</script>
