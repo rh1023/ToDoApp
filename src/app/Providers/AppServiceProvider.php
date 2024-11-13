f260fdb334512f1b21da78a27601790570c7b33b
rh1023 <rh10230810+se@gmail.com>
2024-11-13 11:35:21 +0900

    add：タスク区分操作のためのファイルを作成、機能は未実装のため続きは後程（２へ続く）

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -4,14 +4,24 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\Gate;
+use App\Models\Task;
+use App\Policies\TaskPolicy;
+
+
 class AppServiceProvider extends ServiceProvider
 {
+    protected $policies = [
+        Task::class => TaskPolicy::class,
+    ];
+
     /**
      * Register any application services.
      */
     public function register(): void
     {
         //
+
     }
 
     /**
@@ -20,5 +30,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        // $this->registerPolicies();
     }
 }
